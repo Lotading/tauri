ca67ffeb68490c76ebe0cbff0392aa0215235f46
lotading <aksel.joakim06@gmail.com>
2024-09-06 13:07:09 +0200

    testing features

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,8 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { fetch } from "@tauri-apps/api/http";
 
+const response = await fetch("http://test.tauri.app/data.json", {
+  method: 'GET',
+});
+
+console.log(response.status)
