0ded6bf6d4b9413cdafb2a825b305349be8a06a4
lotading <aksel.joakim06@gmail.com>
2024-09-12 14:21:59 +0200

    small changes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,13 +1,2 @@
 // place files you want to import through the `$lib` alias in this folder.
-import { fetch } from "@tauri-apps/api/http";
-import { invoke } from "@tauri-apps/api";
-
-const testconn = async () => {
-  try {
-    const port = await invoke('get_port')
-    const response = await fetch(`http://localhost:${port}/`)
-    console.log(response)
-  } catch (error){
-    console.error(error)
-  }
-}
+export const testconn from ''
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,17 @@
 <h1>
   main
 </h1>
+
+<script>
+import {testconn} from '$lib/testconn';
+
+export async function get() {
+  try {
+    const response = await testconn();
+    console.log(response)
+  } catch (error) {
+    console.error(error)
+  }
+}
+get()
+</script>
